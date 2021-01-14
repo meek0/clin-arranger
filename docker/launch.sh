58da92711e2e68c231175a7c550d1ce2e90832a6
Patrice Laplante <plaplante@ferlab.bio>
2021-01-14 12:05:08 -0500

    Ensure ES has started prior to launching Arranger

--- docker/launch.sh
+++ docker/launch.sh
@@ -6,5 +6,18 @@ fi
 #if ES not already running
 if [ -z `docker-compose ps -q elasticsearch` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q elasticsearch)` ]; then
     docker-compose up -d --remove-orphans;
-    sleep 5
+    
+    i=15
+    status=`curl --silent --fail http://localhost:9200/donors/_count?q=*`
+    
+    echo "Waiting for ElasticSearch to boot.  Timeout: $i seconds."
+
+    while [ -z "$status" ] && [ $i -gt 0 ]
+    do
+      i=$((i-1))
+      sleep 1
+      status=`curl --silent --fail http://localhost:9200/donors/_count?q=*`
+    done
+
+    [ -z "$status" ] && (echo "ElasticSearch failed to start!"; exit 1;)
 fi
