1bc38aa92b97593a446ddb7e9a251bd60d1b6b7d
Patrice Laplante <plaplante@ferlab.bio>
2021-01-14 10:38:29 -0500

    Start all using npm start

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,11 +1,9 @@
 export DATA_DIRECTORY="$(pwd)/esdata";
 if [ ! -d "$DATA_DIRECTORY" ]; then
-    mkdir -p $DATA_DIRECTORY;
+    tar -xvf testdata.tar.gz esdata 
 fi
 
 #if ES not already running
 if [ -z `docker-compose ps -q elasticsearch` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q elasticsearch)` ]; then
     docker-compose up -d --remove-orphans;
 fi
-
-(cd ../; npm start)
