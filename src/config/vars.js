8e2c1cf2e6400785fd1a01d1553ad8d6c0e6f08c
Patrice Laplante <plaplante@ferlab.bio>
2021-03-05 09:50:16 -0500

    feat: #264 default port if none defined

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,15 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 // import .env variables
-require('dotenv-safe').config({
+require("dotenv-safe").config({
   allowEmptyValues: false,
-  example: './.env.example'
+  example: "./.env.example",
 });
 
 module.exports = {
   env: process.env.NODE_ENV,
-  port: process.env.PORT,
-  logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+  port: process.env.PORT || 5050,
+  logs: process.env.NODE_ENV === "production" ? "combined" : "dev",
   logsRequestInterceptor: process.env.LOGS_REQUEST_INTERCEPTOR,
   authRealm: process.env.AUTH_REALM,
   authServerUrl: process.env.AUTH_SERVER_URL,
@@ -17,5 +17,5 @@ module.exports = {
   sessionSecret: process.env.SESSION_SECRET,
   serviceAccountClientId: process.env.SERVICE_ACCOUNT_CLIENT_ID,
   serviceAccountClientSecret: process.env.SERVICE_ACCOUNT_CLIENT_SECRET,
-  secure: 'false' === process.env.SECURE ? false : true,
+  secure: "false" === process.env.SECURE ? false : true,
 };
