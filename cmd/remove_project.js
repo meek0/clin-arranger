3098df040aebadc12411872aab14c01c9c81f5da
Matt Syms <matthieu.symoens@gmail.com>
2022-09-12 14:24:37 -0400

    feat: CLIN-1361 updated remove project command

--- cmd/remove_project.js
+++ cmd/remove_project.js
@@ -4,19 +4,19 @@ import es from '../services/esClient.js';
 import { PROJECTS_IDS } from '../admin/projectsConfig.js';
 import { ArrangerApi } from '../admin/arrangerApi.js';
 
+const cmdLogger = logger.child({ defaultMeta: { location: 'cmd' } });
+
 const [env] = process.argv.slice(2);
 
 const project = `clin_${env}`;
 
-if (PROJECTS_IDS[project] === undefined) {
+if (!PROJECTS_IDS[project]) {
   throw new Error(`Project "${project}" does not exist`);
 }
 
 const removeArrangerProject = ArrangerApi.removeArrangerProject(es.getInstance());
 
 await removeArrangerProject(PROJECTS_IDS[`clin_${env}`]);
-
-const cmdLogger = logger.child({ defaultMeta: { location: 'cmd' } });
 
 cmdLogger.info(`Deleted project ${project}`);
 
