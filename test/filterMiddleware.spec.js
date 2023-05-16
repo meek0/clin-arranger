008eccfa5c1d8555474c08ec700a16fe552a5d94
Ethienne Roy <eroy@ferlab.bio>
2023-05-16 15:16:39 -0400

    feat: CLIN-1806 added recursion to catch all scenarios

--- test/filterMiddleware.spec.js
+++ test/filterMiddleware.spec.js
@@ -22,42 +22,16 @@ describe("explode boolean multivalues query", () => {
                 "pivot": "donors"
             }
         }
-        booleanFilterMiddleware({body: {variables: initialQuery}}, null, () => {})
-        const expectedResult = {
-            sqon: {
-                "content": [
-                    {
-                        "content": {
-                            "field": "donors.is_hc",
-                            "index": "Variants",
-                            "value": [
-                                "true"
-                            ]
-                        },
-                        "op": "in"
-                    },
-                    {
-                        "content": {
-                            "field": "donors.is_hc",
-                            "index": "Variants",
-                            "value": [
-                                "false"
-                            ]
-                        },
-                        "op": "in"
-                    }
-                ],
-                "op": "and",
-                "pivot": "donors"
-            }
-        }
+        booleanFilterMiddleware({body: {variables: initialQuery}}, null, () => {
+        })
+        const expectedResult = {"sqon":{"content":[{"content":[{"content":{"field":"donors.is_hc","index":"Variants","value":["true"]},"op":"in"},{"content":{"field":"donors.is_hc","index":"Variants","value":["false"]},"op":"in"}],"op":"and"}],"op":"and","pivot":"donors"}}
 
         expect(JSON.stringify(initialQuery)).to.eql(JSON.stringify(expectedResult))
     });
 
     it("shouldn't clean IN query", () => {
         let initialQuery, expectedQuery
-        initialQuery = expectedQuery =  {
+        initialQuery = expectedQuery = {
             "sqon": {
                 "content": [
                     {
@@ -76,8 +50,88 @@ describe("explode boolean multivalues query", () => {
                 "pivot": "donors"
             }
         }
-        booleanFilterMiddleware({body: {variables: initialQuery}}, null, () => {})
+        booleanFilterMiddleware({body: {variables: initialQuery}}, null, () => {
+        })
 
         expect(JSON.stringify(initialQuery)).to.eql(JSON.stringify(expectedQuery))
+    })
+
+    it('should explode this query', () => {
+        const initialQuery = {
+            sqon: {
+                "content": [{
+                    "content": [{
+                        "content": {"field": "donors.patient_id", "value": ["439844"]},
+                        "op": "in"
+                    }], "op": "and"
+                }, {
+                    "content": [{
+                        "content": {
+                            "field": "donors.is_possibly_hc",
+                            "index": "Variants",
+                            "value": ["false", "true"]
+                        }, "op": "all"
+                    }], "op": "and", "pivot": "donors"
+                }], "op": "and", "pivot": "donors"
+            }
+        }
+
+        const expectedQuery = {
+            "sqon": {
+                "content": [
+                    {
+                        "content": [
+                            {
+                                "content": {
+                                    "field": "donors.patient_id",
+                                    "value": [
+                                        "439844"
+                                    ]
+                                },
+                                "op": "in"
+                            }
+                        ],
+                        "op": "and"
+                    },
+                    {
+                        "content": [
+                            {
+                                "content": [
+                                    {
+                                        "content": {
+                                            "field": "donors.is_possibly_hc",
+                                            "index": "Variants",
+                                            "value": [
+                                                "true"
+                                            ]
+                                        },
+                                        "op": "in"
+                                    },
+                                    {
+                                        "content": {
+                                            "field": "donors.is_possibly_hc",
+                                            "index": "Variants",
+                                            "value": [
+                                                "false"
+                                            ]
+                                        },
+                                        "op": "in"
+                                    }
+                                ],
+                                "op": "and"
+                            }
+                        ],
+                        "op": "and",
+                        "pivot": "donors"
+                    }
+                ],
+                "op": "and",
+                "pivot": "donors"
+            }
+        }
+        booleanFilterMiddleware({body: {variables: initialQuery}}, null, () => {
+        })
+        expect(JSON.stringify(initialQuery)).to.eql(JSON.stringify(expectedQuery))
+
     })
 });
