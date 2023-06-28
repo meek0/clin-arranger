2aff84fb52de1286e3569fbaeaac2874d2a3ba29
ethienneroy <ethienner@gmail.com>
2023-06-28 14:49:57 -0400

    feat: CLIN-2011 admin project now using @ferlab arranger (#78)
    feat: CLIN-2011 use forked arranger repo/modules to add middlewares
    
    Co-authored-by: Ethienne Roy <eroy@ferlab.bio>

--- admin/arrangerApi.js
+++ admin/arrangerApi.js
@@ -1,13 +1,13 @@
-import { updateFieldExtendedMapping } from "@arranger/admin/dist/schemas/ExtendedMapping/utils";
+import { updateFieldExtendedMapping } from "@ferlab/arranger-admin/dist/schemas/ExtendedMapping/utils";
 import {
   addArrangerProject,
   removeArrangerProject,
-} from "@arranger/admin/dist/schemas/ProjectSchema/utils";
+} from "@ferlab/arranger-admin/dist/schemas/ProjectSchema/utils";
 import {
   getProjectMetadataEsLocation,
   createNewIndex,
-} from "@arranger/admin/dist/schemas/IndexSchema/utils";
-import { constants } from "@arranger/admin/dist/services/constants";
+} from "@ferlab/arranger-admin/dist/schemas/IndexSchema/utils";
+import { constants } from "@ferlab/arranger-admin/dist/services/constants";
 
 const createNewIndices = async (esClient, confIndices) => {
   const createNewIndexWithClient = createNewIndex(esClient);
