0acfefbb6a02c7b475d9190b322a095fede6486d
Yann MARCOU <creativeyann17@gmail.com>
2024-10-08 13:20:54 -0400

    fix: CLIN-3274 logs + promises

--- app/controllers/beforeSendHandler.js
+++ app/controllers/beforeSendHandler.js
@@ -1,37 +1,44 @@
 import {extractValuesFromSqonByField} from "../utils.js"
+import logger from '../../config/logger.js'
 import { mapVariantToUniqueId, mapVariantPropertiesToVariants, getVariantsProperties } from '../../services/usersApiClient.js';
 
-
 export const cleanupDonors = (variants, patientIds, analysisIds, bioinfoCodes) => {
-    if (patientIds?.length && variants?.length) {
-        // Create a set of patient_ids for faster lookup
-        const patientIdsSet = new Set(patientIds.map(id => String(id)));
-        const analysisIdsSet = new Set(analysisIds.map(id => String(id)));
-        const bioinfoCodesSet = new Set((bioinfoCodes||[]).map(id => String(id)));
+    return new Promise((resolve) => { 
+        var start = Date.now();
+        if (patientIds?.length && variants?.length) {
+            // Create a set of patient_ids for faster lookup
+            const patientIdsSet = new Set(patientIds.map(id => String(id)));
+            const analysisIdsSet = new Set(analysisIds.map(id => String(id)));
+            const bioinfoCodesSet = new Set((bioinfoCodes||[]).map(id => String(id)));
 
-        variants.forEach(variant => {
-            const donors = variant.node.donors?.hits?.edges;
+            variants.forEach(variant => {
+                const donors = variant.node.donors?.hits?.edges;
 
-            if (donors?.length) {
-                const newDonors = donors.filter(d => {
-                    const hasPatientId = patientIdsSet.has(String(d.node.patient_id))
-                    // analysis is optional in both sqon and body response for retro-compatibility
-                    const donorAnalysisId = d.node.analysis_service_request_id
-                    const donorBioinfoCode = d.node.bioinfo_analysis_code
-                    const hasAnalysisId = analysisIdsSet.size === 0 || !donorAnalysisId || analysisIdsSet.has(String(donorAnalysisId))
-                    const hasBioinfoCode = bioinfoCodesSet.size === 0 || !donorBioinfoCode || bioinfoCodesSet.has(String(donorBioinfoCode))
-                    const isValid = hasPatientId && hasAnalysisId && hasBioinfoCode
-                    // console.warn('[variantDonorsHandler]', variant.hash, patientIds, analysisIds, isValid)
-                    return isValid;
-                });
-                variant.node.donors.hits.edges = newDonors;
-                variant.node.donors.hits.total = newDonors.length;
-            }
-        });
-    }
+                if (donors?.length) {
+                    const newDonors = donors.filter(d => {
+                        const hasPatientId = patientIdsSet.has(String(d.node.patient_id))
+                        // analysis is optional in both sqon and body response for retro-compatibility
+                        const donorAnalysisId = d.node.analysis_service_request_id
+                        const donorBioinfoCode = d.node.bioinfo_analysis_code
+                        const hasAnalysisId = analysisIdsSet.size === 0 || !donorAnalysisId || analysisIdsSet.has(String(donorAnalysisId))
+                        const hasBioinfoCode = bioinfoCodesSet.size === 0 || !donorBioinfoCode || bioinfoCodesSet.has(String(donorBioinfoCode))
+                        const isValid = hasPatientId && hasAnalysisId && hasBioinfoCode
+                        // console.warn('[variantDonorsHandler]', variant.hash, patientIds, analysisIds, isValid)
+                        return isValid;
+                    });
+                    variant.node.donors.hits.edges = newDonors;
+                    variant.node.donors.hits.total = newDonors.length;
+                }
+            });
+        }
+        logger.info(`cleanupDonors in ${Date.now() - start} ms`);
+        resolve(null); 
+    });
+    
 };
 
 async function fetchFlags (req, variants) {
+    var start = Date.now();
     try {
         if (variants?.length) {
             const uniqueIds = variants.map(mapVariantToUniqueId).filter(id => !!id);
@@ -39,18 +46,27 @@ async function fetchFlags (req, variants) {
             mapVariantPropertiesToVariants(variants, variantProperties);
         }
     } catch(e) {
-        console.error('Failed to fetch variant flags: ' + e);
+        logger.error('Failed to fetch variant flags: ' + e);
 
         if (variants?.length) {
             variants.forEach(variant => variant.node.flags = []); // ensure empty flags
         }
+    } finally {
+        logger.info(`fetchFlags in ${Date.now() - start} ms`);
     }
 }
 
-
+async function parseResponseBody (arg) {
+    return new Promise(resolve => {
+        var start = Date.now();
+        const data = JSON.parse(arg);
+        logger.info(`parseResponseBody in ${Date.now() - start} ms`);
+        resolve(data);
+    });
+}
 
 export default async function(req, res, next) {
-
+    var start = Date.now();
     const sqon = req.body?.variables?.sqon;
 
     const patientIds = extractValuesFromSqonByField(sqon, 'donors.patient_id')
@@ -63,16 +79,17 @@ export default async function(req, res, next) {
         const originalSend = res.send;
         res.send = async function () { // function is mandatory, () => {} doesn't work here
             // parse variants from response body
-            const data = JSON.parse(arguments[0]);
+            const data = await parseResponseBody(arguments[0]);
             const variants = data?.data?.Variants?.hits?.edges || data?.data?.cnv?.hits?.edges;
-            cleanupDonors(variants, patientIds, analysisIds, bioinfoCodes)
+            await cleanupDonors(variants, patientIds, analysisIds, bioinfoCodes)
             if (withFlags) {
                 await fetchFlags(req, variants)
             }
             // override response body with modified data
             arguments[0] = JSON.stringify(data);
             originalSend.apply(res, arguments);
+            logger.info(`[${req.method}] ${res.statusCode} ${req.url} in ${Date.now() - start} ms`);
         };
     }
-    return next();
+    next();
 }
