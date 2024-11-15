1fc0aed07ed16c0e44eed955760358fd01bb9980
Yann MARCOU <creativeyann17@gmail.com>
2024-11-15 13:35:08 -0500

    fix: CLIN-3510 bump arranger + logs

--- app/middlewares/afterES/variantDonors.js
+++ app/middlewares/afterES/variantDonors.js
@@ -1,13 +1,16 @@
 // extract the inner_hits from ES response if available and copy its content where is should be normally:
 // - inner_hits.donors (is from beforeES/nestedDonors)
 // - (add more inner_hits bellow ...)
-export default function (_, hits) {
+export default function (body, hits) {
     if (hits?.hits) {
         hits.hits = hits.hits?.map((hit) => {
             if (hit?.inner_hits) {
                 const innerHitsDonors = hit?.inner_hits?.donors?.hits?.hits?.map((h)=> h._source)
                 // don't replace donors if already exist for complex nested queries
-                if (innerHitsDonors && innerHitsDonors.length > 0 && !hit._source.donors) {
+                if (hit._source?.donors?.length) {
+                    console.warn('[variantDonors]', hit._source?.donors?.length, innerHitsDonors.length)
+                }
+                if (innerHitsDonors?.length && !hit._source?.donors) {
                     hit._source.donors = structuredClone(innerHitsDonors);
                 }
                 delete hit.inner_hits;
